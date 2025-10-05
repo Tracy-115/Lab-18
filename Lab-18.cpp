6937f9a18b9bfbc63d55e4fd02a8f584c3422667
Tracy-115 <kmin984@insite.4cd.edu>
2025-10-05 11:52:28 -0700

    Update Lab-18.cpp

--- Lab-18.cpp
+++ Lab-18.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Movie{
+  float rating;
+  string comment;
+  Movie *next;
+}
+
+int main{
+  Movie *head=nullptr;
+  
+  char decision;
+  while (toupper(again) == 'Y') {
+      float rating;
+      string decision;
+
+      cout << "Enter review rating 0-5: ";
+      cin >> rating;
+      cout << "Enter review comments: ";
+      getline(cin, desision);
 
