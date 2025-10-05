90cc668faf55ef62df866b46078a4aab5963f534
Tracy-115 <kmin984@insite.4cd.edu>
2025-10-05 12:02:23 -0700

    Update Lab-18.cpp

--- Lab-18.cpp
+++ Lab-18.cpp
@@ -6,13 +6,20 @@ struct Movie{
   float rating;
   string comment;
   Movie *next;
-}
+};
 
 int main{
   Movie *head=nullptr;
+
+  int choice;
+  cout << "Which linked list method should we use?\n";
+  cout << "    [1] New nodes are added at the head of the linked list\n"<< "    [2] New nodes are added at the tail of the linked list\n";
+  cout << "    Choice: ";
+  cin >> choice;
+  cin.ignore();
   
-  char decision;
-  while (toupper(again) == 'Y') {
+  char decision = 'Y';
+  while (toupper(decision) == 'Y') {
       float rating;
       string decision;
 
@@ -20,4 +27,30 @@ int main{
       cin >> rating;
       cout << "Enter review comments: ";
       getline(cin, desision);
+      if (choice = 1){
+        Movie (head, rating, comment);
+      }
+      else{
+        Movie (tain, rating, comment);
+      }
+      cout << "Enter another review? Y/N: ";
+      cin >> again;
+      cin.ignore();
+  }
+void Head(Movie *&head, float rating, string &comment){
+  Movie *newNode = new Movie;
+  newNode->rating=rating;
+  newNode->comment=comment;
+  newNode->head = head;
+  head = newNode;
+}
+
+
+
+
+
+
+
+
+
 
